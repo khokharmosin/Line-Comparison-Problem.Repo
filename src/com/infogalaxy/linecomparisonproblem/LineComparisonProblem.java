557694f2253b55e045c6b9ed595508957fbaf4f1
khokharmosin <khokharmosin7806@gmail.com>
2022-03-11 10:49:26 +0530

    ADDED: Line Comparison Problem

---
+++ src/com/infogalaxy/linecomparisonproblem/LineComparisonProblem.java
@@ -0,0 +1,6 @@
+package com.infogalaxy.linecomparisonproblem;
+public class LineComparisonProblem {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Line Comprasion Computation Program On Master Branch");
+    }
+}
